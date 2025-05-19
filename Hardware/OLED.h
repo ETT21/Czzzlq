7fe799ed484ac868d4be9f549a63bbd89cdfbb70
ETT21 <149931632+ETT21@users.noreply.github.com>
2025-05-19 22:27:03 +0800

    Create OLED.h

---
+++ Hardware/OLED.h
@@ -0,0 +1,10 @@
+PS F:\STM32\智能天气时钟\TFT\TFT显示> git push -u origin main
+To https://github.com/ETT21/Czzzlq.git
+ ! [rejected]        main -> main (fetch first)
+error: failed to push some refs to 'https://githups://github.com/ETT21/Czzzlq.git'
+hint: Updates were rejected because the remote contains work that you do
+hint: not have locally. This is usually caused by another repository pushing
+hint: to the same ref. You may want to first integrate the remote changes
+hint: (e.g., 'git pull ...') before pushing again.
+hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+PS F:\STM32\智能天气时钟\TFT\TFT显示>
